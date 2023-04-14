f1113d4a14fcd658fc7f2570bc7f2354793bfb83
Nik <nikitama1hr@gmail.com>
2023-04-15 00:12:50 +0300

    add some test

--- sundaes/src/App.test.js
+++ sundaes/src/App.test.js
@@ -1,6 +1,7 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 import FormCheckInput from "react-bootstrap/FormCheckInput";
+import userEvent from "@testing-library/user-event";
 
 test('check input on checked', () => {
   render(<App/>)
@@ -8,4 +9,18 @@ test('check input on checked', () => {
   expect(checkbox).not.toBeChecked()
   fireEvent.click(checkbox)
   expect(checkbox).toBeEnabled()
+})
+
+//mock test
+
+test('update scoop', async () => {
+  render(<Options optionType="scoops"/>)
+
+  const scSubtiotal = screen.getByText('Scoops tototal: $')
+  expect(scSubtiotal).toHaveTextContent('0,00')
+
+  const vanillaInput = await screen.findAllByRole('spinbutton', {
+    name: 'Vanilla'
+  })
+  userEvent.clear(vanillaInput)
 })
